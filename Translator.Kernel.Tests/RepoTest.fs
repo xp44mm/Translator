1816edd0a3a089cc464d37d5075931ed9d7ea0f4
xp44mm <34696643@qq.com>
2022-07-09 16:43:07 +0800

    font family

--- Translator.Kernel.Tests/RepoTest.fs
+++ Translator.Kernel.Tests/RepoTest.fs
@@ -11,7 +11,7 @@ open Translator.ef
 //[<Collection("AutofacCollection")>]
 type RepoTest(output: ITestOutputHelper) = //,fixture: AutofacFixture
     //let di = fixture.Container
-
+    let x = ()
     //[<Fact>]
     //member this.getWords() =
     //    Assert.NotNull(di)
@@ -28,28 +28,32 @@ type RepoTest(output: ITestOutputHelper) = //,fixture: AutofacFixture
         //    output.WriteLine(sprintf "%O" w)
         //output.WriteLine("")
 
-    [<Fact>]
-    member this.insert() =
-        let repo = new WordRepo()
+    //[<Fact>]
+    //member this.insert() =
+    //    let repo = new WordRepo()
 
-        let word = new Word(English = "csl.translate.test.data",Chinese = "´ŞÊ¤Àû·­Òë²âÊÔÊı¾İ")
-        repo.update(word)
-        let w1 = repo.getWords() |> Array.find(fun w -> w.English = "csl.translate.test.data")
-        Assert.Equal(w1.Chinese,"´ŞÊ¤Àû·­Òë²âÊÔÊı¾İ")
+    //    let word = new Word(English = "csl.translate.test.data",Chinese = "´ŞÊ¤Àû·­Òë²âÊÔÊı¾İ")
+    //    repo.update(word)
+    //    let w1 = 
+    //        repo.getWords() 
+    //        |> Array.find(fun w -> w.English = "csl.translate.test.data")
+    //    Assert.Equal(w1.Chinese,"´ŞÊ¤Àû·­Òë²âÊÔÊı¾İ")
 
-    [<Fact>]
-    member this.update() =
-        let repo = new WordRepo()
+    //[<Fact>]
+    //member this.update() =
+    //    let repo = new WordRepo()
 
-        let word = new Word(English = "csl.translate.test.data",Chinese = "´ŞÊ¤Àû.·­Òë.²âÊÔ.Êı¾İ")
-        repo.update(word)
-        let w1 = repo.getWords() |> Array.find(fun w -> w.English = "csl.translate.test.data")
-        Assert.Equal(w1.Chinese,"´ŞÊ¤Àû.·­Òë.²âÊÔ.Êı¾İ")
+    //    let word = new Word(English = "csl.translate.test.data",Chinese = "´ŞÊ¤Àû.·­Òë.²âÊÔ.Êı¾İ")
+    //    repo.update(word)
+    //    let w1 = 
+    //        repo.getWords() 
+    //        |> Array.find(fun w -> w.English = "csl.translate.test.data")
+    //    Assert.Equal(w1.Chinese,"´ŞÊ¤Àû.·­Òë.²âÊÔ.Êı¾İ")
 
-    [<Fact>]
-    member this.delete() =
-        let repo = new WordRepo()
-        repo.delete("csl.translate.test.data")
-        let w1 = repo.getWords() |> Array.tryFind(fun w -> w.English = "csl.translate.test.data")
-        Assert.Equal(w1,Option.None)
+    //[<Fact>]
+    //member this.delete() =
+    //    let repo = new WordRepo()
+    //    repo.delete("csl.translate.test.data")
+    //    let w1 = repo.getWords() |> Array.tryFind(fun w -> w.English = "csl.translate.test.data")
+    //    Assert.Equal(w1,Option.None)
     
